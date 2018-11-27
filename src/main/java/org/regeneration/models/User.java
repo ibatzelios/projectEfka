547588a908e57eca0da05668c1a9d7f0f3013775
Eleni Palousi <elenipalou@gmail.com>
2018-11-27 19:16:34 +0200

    Return loggedin Doctor

--- src/main/java/org/regeneration/models/User.java
+++ src/main/java/org/regeneration/models/User.java
@@ -1,10 +1,7 @@
 package org.regeneration.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 public interface User {
-
-
+    
     public String getPassword();
     public String getUsername();
 }
